50ace2ca6ba387413b48024b70627389168989af
grishindn <69839479+grishindn@users.noreply.github.com>
2021-11-27 22:04:23 +0300

    Update main.py

--- lab1/main.py
+++ lab1/main.py
@@ -2,7 +2,7 @@ import math
 
 
 def calculation(x):
-    t = math.log(math.sqrt(abs(x - 2)) + 1.2) / (2 + math.e ** x) + (2 / x) ** (1 / 3)
+    t = math.log(math.sqrt(abs(x - 2)) + 1.2) / (2 + math.e**x) + (2 / x)**(1 / 3)
     return t
 
 
