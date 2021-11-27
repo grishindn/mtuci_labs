f52217064028091cbadbcd7d62c1581355f704be
grishindn <69839479+grishindn@users.noreply.github.com>
2021-11-27 22:03:57 +0300

    Add files via upload
    Lab 1

---
+++ lab1/main.py
@@ -0,0 +1,12 @@
+import math
+
+
+def calculation(x):
+    t = math.log(math.sqrt(abs(x - 2)) + 1.2) / (2 + math.e ** x) + (2 / x) ** (1 / 3)
+    return t
+
+
+print("Введите Х: ")
+x = float(input())
+f = calculation(x)
+print(f)
